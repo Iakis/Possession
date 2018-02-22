ae51b95a07c1d6d549213ec7d8222008843c378d
Meeral Qureshi <meeralqureshi@hotmail.com>
2018-02-22 17:52:44 -0500

    Configured the IzaOni script to use the decay bar (But need to drag and drop it in

--- Possession/Assets/Scripts/Characters/IzaOni.cs
+++ Possession/Assets/Scripts/Characters/IzaOni.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IzaOni : Izanami {
 
@@ -8,6 +9,10 @@ public class IzaOni : Izanami {
     static IzaOni s_izaoni;
     static Izanagi m_izanagi;
     new public float speed = 5;
+	float decay_amount = 30;
+	float time;
+	public Image DecayBar;
+	public GameObject healthCanvas;
 
     // Use this for initialization
     void Start () {
@@ -15,6 +20,7 @@ public class IzaOni : Izanami {
         m_izanagi = Izanagi.Get();
         form = "ghost";
         following = false;
+		healthCanvas.SetActive (true);
     }
 
     new public static IzaOni Get()
@@ -30,6 +36,10 @@ public class IzaOni : Izanami {
     // Update is called once per frame
     public override void Update () {
         base.Update();
+		if (time > 0) {
+			time -= Time.deltaTime;
+			DecayBar.fillAmount = time / decay_amount;
+		}
 
     }
 
