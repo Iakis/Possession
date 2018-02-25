4be534f1927897d2d1093ff46db3b2ef0dd8b880
# <#>
2018-02-25 03:44:31 -0500

    visual effects, directory clean up

---
+++ Possession/Assets/bladetrail.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bladetrail : MonoBehaviour {
+
+    public static bladetrail s_bladetrail;
+
+    public static bladetrail Get()
+    {
+        return s_bladetrail;
+    }
+
+    public bool isAttacking;
+
+    bladetrail()
+    {
+        s_bladetrail = this;
+    }
+    void Start () {
+        isAttacking = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (isAttacking)
+        {
+            this.gameObject.GetComponent<TrailRenderer>().enabled = true;
+        } else
+        {
+            this.gameObject.GetComponent<TrailRenderer>().enabled = false;
+        }
+	}
+}
