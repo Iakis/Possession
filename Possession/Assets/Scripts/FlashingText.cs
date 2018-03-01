57d0107a46ce953518764b893165138af75ce8e1
Meeral Qureshi <meeralqureshi@hotmail.com>
2018-03-01 00:00:58 -0500

    Added flashing title screen text

---
+++ Possession/Assets/Scripts/FlashingText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashingText : MonoBehaviour {
+
+	Text startGame;
+	string blank = "";
+	string flashing = "Press space to play";
+
+	// Use this for initialization
+	void Start () {
+		startGame = GetComponent<Text> ();
+		StartCoroutine (BlinkText ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//function to blink the text 
+	public IEnumerator BlinkText(){
+		//blink it forever. You can set a terminating condition depending upon your requirement
+		while (true) {
+			startGame.text = blank;
+			yield return new WaitForSeconds (.5f);
+			startGame.text = flashing;
+			yield return new WaitForSeconds (.5f);
+		}
+	}
+}
