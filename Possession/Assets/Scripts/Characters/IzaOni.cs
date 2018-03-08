1fc572e62a7f961793490f697ec4e219634f7118
# <#>
2018-03-08 06:58:50 -0500

    resourfces

--- Possession/Assets/Scripts/Characters/IzaOni.cs
+++ Possession/Assets/Scripts/Characters/IzaOni.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 public class IzaOni : Izanami {
 
@@ -148,7 +147,7 @@ public class IzaOni : Izanami {
         if (Input.GetButtonUp("NamiInteract"))
         {
             oanim.SetTrigger("die");
-            GameObject go = (GameObject)Instantiate(AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Izanami.prefab", typeof(GameObject)), this.gameObject.transform.position, this.gameObject.transform.rotation);
+            GameObject go = (GameObject)Instantiate(Resources.Load("Izanami", typeof(GameObject)), this.gameObject.transform.position, this.gameObject.transform.rotation);
             this.gameObject.GetComponent<IzaOni>().enabled = false;
             this.gameObject.GetComponent<OniAI>().enabled = true;
             m_axe.GetComponent<BoxCollider>().enabled = false;
