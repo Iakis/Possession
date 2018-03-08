c3609ff96eaf7247e86fdcd27b4e30efa4e85bb6
# <#>
2018-03-08 00:50:02 -0500

    trail

---
+++ Possession/Assets/Scripts/Characters/bladetrail2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bladetrail2 : MonoBehaviour
+{
+
+    public static bladetrail m_bladetrail;
+
+    void Start()
+    {
+        m_bladetrail = bladetrail.Get();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_bladetrail.isAttacking)
+        {
+            this.gameObject.GetComponent<TrailRenderer>().enabled = true;
+        }
+        else
+        {
+            this.gameObject.GetComponent<TrailRenderer>().enabled = false;
+        }
+    }
+}
